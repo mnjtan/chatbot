cb4a49dcb13916b29bd173897b47dd1f23b00eec
mario ruiz <mario.d.ruiz2@gmail.com>
2018-04-21 23:28:24 -0400

    add program flow

---
+++ ChatBotClient/src/bot/bot.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { BotComponent } from "./bot.component";
+
+@NgModule({
+    declarations: [
+        BotComponent
+    ]
+})
+export class BotModule{
+
+}
--- ChatBotClient/src/login/login.component.ts
+++ ChatBotClient/src/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input, EventEmitter, Output } from "@angular/core";
+import { User } from "./user";
+import { DataService } from "../services/data.service";
 
 
 @Component({
@@ -7,5 +9,55 @@ import { Component } from "@angular/core";
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent{
-    
+
+    @Output() notifyState: EventEmitter<string> = new EventEmitter<string>();
+    @Output() notifyLoggedIn: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    loggedIn: boolean;
+    user: User;
+    verifiedUser: User;
+    dataClient: DataService;
+    Error: string;
+
+    constructor(dataClient: DataService){
+        this.loggedIn = false;
+        this.user = new User();
+        this.verifiedUser = new User();
+        this.dataClient = dataClient;
+        this.Error = null;
+    }
+
+    setUser(data: Object){
+        if (data == null){
+            this.verifiedUser = null;
+            this.Error = "We do not recognize your email and/or password. Please try again or Register for an account.";
+            return;
+        }
+        this.verifiedUser = {
+            userId: data['userId'],
+            email: data['email'],
+            name: data['name'],
+            phone: data['phone'],
+            team: data['team']
+        }
+        //alert container about sign in, hide signin / show bot
+        this.notifyState.emit("bot");
+        this.notifyLoggedIn.emit(true);
+    }
+
+    submit(){
+        this.Error = null;
+
+        //call the service
+        this.dataClient.signIn(this.user.email).subscribe(data => {
+            this.setUser(data);
+            console.log("second",this.verifiedUser);
+        });
+    }
+
+    guestSubmit(){
+        console.log("Continue as guest");
+        this.notifyState.emit("bot");
+    }
+
 }
