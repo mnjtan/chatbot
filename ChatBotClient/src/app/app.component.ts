04dc0810182842a5ab6b62018cd4bd458f466a3c
mario ruiz <mario.d.ruiz2@gmail.com>
2018-04-22 01:24:25 -0400

    hacked bot window to not show

--- ChatBotClient/src/app/app.component.ts
+++ ChatBotClient/src/app/app.component.ts
@@ -20,12 +20,15 @@ export class AppComponent {
     if(typeof message === 'string'){
       this.state = message;
       var x = document.getElementById('botwindow');
+      var inner = document.getElementById("innerDiv");
       if(message === 'bot') {
         x.style.visibility = 'visible';
+        inner.className = "col-md-offset-3 col-md-7"; 
       }
       else
       {
         x.style.visibility = 'hidden';
+        inner.className = "col-md-offset-3 col-md-1"; 
       }
     }
     else if(typeof message === 'boolean'){
@@ -37,15 +40,24 @@ export class AppComponent {
   goSignUp()
   {
     this.state = "register"
+    var x = document.getElementById('botwindow');
+    x.style.visibility = 'hidden';
+    document.getElementById("innerDiv").className = "col-md-offset-3 col-md-1";
   }
 
   goLogin(){
     this.state = "login"
+    var x = document.getElementById('botwindow');
+    x.style.visibility = 'hidden';
+    document.getElementById("innerDiv").className = "col-md-offset-3 col-md-1";
   }
 
   goSignOut(){
     this.state = "login"
     this.loggedIn = false;
+    var x = document.getElementById('botwindow');
+    x.style.visibility = 'hidden';
+    document.getElementById("innerDiv").className = "col-md-offset-3 col-md-1";
   }
 
 }
